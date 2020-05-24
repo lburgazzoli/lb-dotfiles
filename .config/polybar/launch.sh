3f3fe3343844b68a32c9753af6a9c7ceee4188a3
lburgazzoli <lburgazzoli@gmail.com>
2020-05-24 13:19:41 +0200

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,5 +3,6 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r main &
-
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload $m &
+done
