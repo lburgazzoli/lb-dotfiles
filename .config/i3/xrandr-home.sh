130d6fa4008008767525409d3775f95e8ef18602
Luca Burgazzoli <lburgazzoli@gmail.com>
2020-12-12 11:52:39 +0100

    new monitor

--- .config/i3/xrandr-home.sh
+++ .config/i3/xrandr-home.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#xrandr --ou.tput DP-3.1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-6 --off --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
-xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-1-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-1-1 --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off
